765a30b28e98ae4ba198246f0fa9680948c8941e
maxtwis <napat.trs@gmail.com>
2025-11-03 15:31:41 +0700

    Update station-history.js

--- js/station-history.js
+++ js/station-history.js
@@ -242,6 +242,7 @@ export async function renderStationHistoryChart(stationUid, hours = 24) {
                   day: 'numeric',
                   hour: '2-digit',
                   minute: '2-digit',
+                  hour12: false,
                 });
               },
               label: (context) => {
